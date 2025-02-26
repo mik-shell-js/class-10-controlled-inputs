f4adde668cf7f5a49f0830ba0492f1637653dcb8
Kyle J. Harms <harmsk@users.noreply.github.com>
2025-02-26 16:28:30 +0000

    in class example

--- src/components/ImageSelector.jsx
+++ src/components/ImageSelector.jsx
@@ -1,16 +1,16 @@
-export default function ImageSelector() {
+export default function ImageSelector({ onImageChange }) {
   const blackSeal = '/images/cornell-seal-black.svg'
   const redSeal = '/images/cornell-seal-red.svg'
 
   return (
     <div className="selector">
-      <button>
+      <button onClick={() => onImageChange(blackSeal)}>
         <img src={blackSeal} />
       </button>
-      <button>
+      <button onClick={() => onImageChange(redSeal)}>
         <img src={redSeal} />
       </button>
-      <button>
+      <button onClick={() => onImageChange(undefined)}>
         <em>None</em>
       </button>
     </div>
